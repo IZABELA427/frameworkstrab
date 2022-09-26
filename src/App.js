70e00edbbb3a39baba86ae09a6c4eb0aa33d55c6
IZABELA427 <izabela.santana@estudante.ifms.edu.br>
2022-09-26 17:09:47 -0400

    Envio correção

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import { Component } from 'react';
+import {Buscar} from './components/Buscar'
+import {Cabecalho} from './components/Cabecalho'
+import { Listar } from './components/Listar';
+// import { Rodape } from './Components/Rodape';
+
+
+class App extends Component{
+  state = {
+      busca: '',
+      odas: []
+  }
+
+  componentDidMount() {
+    this.carregaODAS();
+  
+  }
+
+  carregaODAS() {
+    const {busca} = this.state;
+    fetch('https://www.bocaweb.com.br/apibocaweb?nome='+busca)
+    .then(response => response.json())
+    .then(odas => this.setState({odas}))
+  }
+
+  buscaODA = (evento) => {
+    this.setState({busca: evento.target.value})
+    this.carregaODAS()
+  }
+
+  render(){
+    const {busca, odas} = this.state;
+    return(
+      <section>
+
+        <Cabecalho />
+        <Buscar 
+        busca = {this.state.busca}
+        buscaODA = {this.buscaODA}
+        />
+
+      <Listar
+      odas = {odas}
+      />
+      {/* <Rodape/> */}
+      </section>
+    )
+  }
+
+}
+
+export default App;
---
+++ src/components/Cabecalho/index.jsx
@@ -0,0 +1,3 @@
+export const Cabecalho = () =>{
+    <p>Bocaweb front</p>
+}
